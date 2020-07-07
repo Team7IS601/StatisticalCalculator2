cf6c1de6c6e30d3df36a7c2152303f645b41f497
Adam Adamson <aa748@njit.edu>
2020-07-07 19:52:11 -0400

    Task- Added setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='Statistic Calculator',
+      version='0.0.1',
+      description='Its a calculator',
+      author='Adam, Ryan, Michael',
+      author_email='',
+      url='',
+      packages=find_packages(),
+     )
