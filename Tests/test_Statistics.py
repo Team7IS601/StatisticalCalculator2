d9a492d4f40dfbbce1bb5136a4ffd2fbb71bf402
mam32 <mam326@njit.edu>
2020-07-08 15:31:52 -0400

    Feature: Added MODE to module

--- Tests/test_Statistics.py
+++ Tests/test_Statistics.py
@@ -5,6 +5,8 @@ from CsvReader.CsvReader3 import getFileData
 class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.statistics = Statistics()
+        test_answer = getFileData('Tests/Data/UnitTestStatistics.csv').data
+        sample_data = getFileData('Tests/Data/TestSampleData.csv').data
 
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.statistics, Statistics)
@@ -22,7 +24,11 @@ class MyTestCase(unittest.TestCase):
     #         self.assertEqual(self.statistics.median(self.column1), float(row['median']))
     #         self.assertEqual(self.statistics.result, float(row['median']))
 
-
+    def test_mode_statistics(self):
+        for row in self.test_answer:
+            pprint(row["mode"])
+        self.assertEqual(self.statistics.mode(self.column1), float(row['mode']))
+        self.assertEqual(self.statistics.result, float(row['mode']))
 
 if __name__ == '__main__':
     unittest.main()
