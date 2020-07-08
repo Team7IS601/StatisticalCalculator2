fa7bd3d3222fe492fa5b59e31f54225675d729dc
mam32 <mam326@njit.edu>
2020-07-08 14:42:29 -0400

    Fix: Line had two references to statistics, replaced with "self"

--- Tests/test_Statistics.py
+++ Tests/test_Statistics.py
@@ -4,7 +4,7 @@ from Stats.Statistics import Statistics
 class MyTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.Statistics = Statistics('/Tests/Data/UnitTestStatsAnswers.csv')
+        self.Statistics = self('/Tests/Data/UnitTestStatsAnswers.csv')
 
     def test_instantiate_statcalculator(self):
         self.assertIsInstance(self.Statistics, Statistics)
