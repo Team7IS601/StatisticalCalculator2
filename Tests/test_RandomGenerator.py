c2f473b2ae7966a604d0fb5c517a18ef414c87f2
mam32 <mam326@njit.edu>
2020-07-18 23:04:53 -0400

    Task: Created Modules for Random Generators

--- Tests/test_RandomGenerator.py
+++ Tests/test_RandomGenerator.py
@@ -1,1 +1,22 @@
-from R
+from RandomGenerator.random import random
+from pprint import pprint
+from Stats.RandomGenerators import list_generator
+import Stats.RandomGenerators
+
+def test_something(self):
+    # List evaluates to False if empty
+    test_list = Stats.RandomGenerators.list_generator()
+    self.assertTrue(test_list)
+
+
+def test_random_item(self):
+    # test default list count
+    test_list = Stats.RandomGenerators.list_generator()
+    self.assertEqual(len(test_list), 100)
+
+
+def test_random_seed(self):
+    seed_val = 6
+    choice = Stats.RandomGenerators.random_seed(seed=seed_val)
+    self.result = seed_val
+    self.assertEqual(self.result, 6)
