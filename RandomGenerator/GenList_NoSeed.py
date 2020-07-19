bf5379e4edb06ba26087ce3b0bd940ba9b153ce4
mam32 <mam326@njit.edu>
2020-07-19 19:16:09 -0400

    Task: Created GenList No Seed, Tested Module By Itself, somethings wrong, fixing

---
+++ RandomGenerator/GenList_NoSeed.py
@@ -0,0 +1,26 @@
+#Generate a random number without a seed between a range of two numbers - Both Integer and Decimal
+from random import randint
+import random
+
+
+class GenListNoSeed:
+    def list_int (x, y, ranges):
+        if isinstance(x, float):
+            return GenListNoSeed.list_float(x, y, ranges)
+        series = []
+        for each in range(ranges):
+            number = randint(x, y)
+            series.append(number)
+        return series
+
+
+    def list_float(x, y, ranges):
+        series = []
+
+        for each in range (ranges):
+            number = random.uniform(x, y)
+            series.append(number)
+        return series
+
+
+print(GenListNoSeed.list_int(4.4,9.7,10))
