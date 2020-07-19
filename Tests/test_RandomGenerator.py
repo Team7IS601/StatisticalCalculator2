8be2f75c95fa9679e3d12ea0b49e8cee65e554fd
mam32 <mam326@njit.edu>
2020-07-19 19:29:12 -0400

    Task: Reworked Random Test, somethings wrong.  Fixing.

--- Tests/test_RandomGenerator.py
+++ Tests/test_RandomGenerator.py
@@ -1,22 +1,10 @@
-from RandomGenerator.random import random
-from pprint import pprint
-from Stats.RandomGenerators import list_generator
-import Stats.RandomGenerators
-
-def test_something(self):
-    # List evaluates to False if empty
-    test_list = Stats.RandomGenerators.list_generator()
-    self.assertTrue(test_list)
+import unittest
+import Random
+# from pprint import pprint
+from Random.GenList_WithSeed import GenListWithSeed
+# from Random.GenList_NoSeed import GenListNoSeed
+# from Random.GenNum_NoSeed import GenNumNoSeed
+# from Random.GenNum_WithSeed import GenNumWithSeed
 
 
-def test_random_item(self):
-    # test default list count
-    test_list = Stats.RandomGenerators.list_generator()
-    self.assertEqual(len(test_list), 100)
-
-
-def test_random_seed(self):
-    seed_val = 6
-    choice = Stats.RandomGenerators.random_seed(seed=seed_val)
-    self.result = seed_val
-    self.assertEqual(self.result, 6)
+print(GenListWithSeed.list_int(1, 10, 5, 4))
