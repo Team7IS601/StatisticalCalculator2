40ce82c0caf09b6dbc8b7feda1f3d4942289963f
mam32 <mam326@njit.edu>
2020-07-19 19:33:54 -0400

    Fix: Figured it out, I mixed up the ranges and the name of the dir for modules was wrong.  Done, fixed. Tests ok now.

--- Tests/test_RandomGenerator.py
+++ Tests/test_RandomGenerator.py
@@ -1,10 +1,14 @@
-import unittest
-import Random
-# from pprint import pprint
-from Random.GenList_WithSeed import GenListWithSeed
-# from Random.GenList_NoSeed import GenListNoSeed
-# from Random.GenNum_NoSeed import GenNumNoSeed
-# from Random.GenNum_WithSeed import GenNumWithSeed
+from RandomGenerator.GenList_WithSeed import GenListWithSeed
+from RandomGenerator.GenList_NoSeed import GenListNoSeed
+from RandomGenerator.GenNum_NoSeed import GenNumNoSeed
+from RandomGenerator.GenNum_WithSeed import GenNumWithSeed
 
+print()
 
-print(GenListWithSeed.list_int(1, 10, 5, 4))
+if __name__ == '__main__':
+    print(GenListWithSeed.list_int(1, 10, 5, 4), "Tested List With Seed")
+    print(GenListNoSeed.list_int(1, 10, 5), "Tested List With NO Seed")
+    print(GenNumNoSeed.num_int(1, 10), "Tested Number NO Seed - Interger")
+    print(GenNumNoSeed.num_int(1.4, 10.4), "Tested Number NO Seed -- Float")
+    print(GenNumWithSeed.num_int(1, 10, 5), "Tested Number With Seed - Interger")
+    print(GenNumWithSeed.num_int(1.1, 10.4, 5), "Tested Number With Seed - Float")
